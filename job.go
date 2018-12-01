f412acfe2a5fec8b727f35ece4cf232d1024d14d
kim yongbin <kybinz@gmail.com>
2018-12-01 16:16:08 +0900

    Add commend for job and task

---
+++ job.go
@@ -0,0 +1,5 @@
+package coco
+
+// Job consists of tasks.
+// A job is actually a root task holds default options for child tasks.
+// Child task could overrride the default options.
---
+++ task.go
@@ -0,0 +1,8 @@
+package coco
+
+// Task is a task handled by a worker.
+// A task is either a parent task or a child task.
+// A parent task could define how child tasks are excuted. (parrellel or series)
+// A parent task could hold 'pre-command', 'post-command', 'pre-task-command', 'post-task-command'.
+// Note that the environments from the commands will not inherited.
+// A Child holds series of (but usually one) commands.
