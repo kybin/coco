4399750c6aabde7cbdad476d063ffd356324e6e3
kim yongbin <kybinz@gmail.com>
2018-12-01 16:29:34 +0900

    enhance task comment

--- task.go
+++ task.go
@@ -2,7 +2,9 @@ package coco
 
 // Task is a task handled by a worker.
 // A task is either a parent task or a child task.
+//
 // A parent task could define how child tasks are excuted. (parrellel or series)
 // A parent task could hold 'pre-command', 'post-command', 'pre-task-command', 'post-task-command'.
-// Note that the environments from the commands will not inherited.
-// A Child holds series of (but usually one) commands.
+// Note that the environments from the commands will not inherited. Use 'env' for that.
+//
+// A Child holds series of (usually one) commands.
