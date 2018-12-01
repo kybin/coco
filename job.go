3b767daff035714848d8ab5856215e619b01175c
kim yongbin <kybinz@gmail.com>
2018-12-01 17:05:04 +0900

    errata

--- job.go
+++ job.go
@@ -2,4 +2,4 @@ package coco
 
 // Job consists of tasks.
 // A job is actually a root task holds default options for child tasks.
-// Child task could overrride the default options.
+// Child task could override the default options.
