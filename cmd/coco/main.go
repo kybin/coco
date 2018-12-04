37dd750cbbb357ef49d67ebc15cce73afa074a1e
kim yongbin <kybinz@gmail.com>
2018-12-04 21:46:52 +0900

    add job id comment to coco(client)

--- cmd/coco/main.go
+++ cmd/coco/main.go
@@ -2,3 +2,7 @@ package main
 
 // Client creates a job and send it to server.
 // It will only send a job and exit, do not wait until the job is done.
+//
+// It upload a job as json file to cocoserver, and got the job id.
+// Client does not know what will be the job id before submitted,
+// so it will be empty.
