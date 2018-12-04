06c2eab574882c15d55835153d727b3f38cc8470
kim yongbin <kybinz@gmail.com>
2018-12-04 22:01:46 +0900

    a server send it's task to a worker over rpc

--- cmd/cocoserver/main.go
+++ cmd/cocoserver/main.go
@@ -5,7 +5,7 @@ package main
 //
 // When it got a job from a client, it will split the job to tasks.
 // it will look available workers and pick one randomly. (or just next one?)
-// It sends a task to the worker, and waiting until the task is done by the worker.
+// It sends a task to the worker via rpc, and waiting until the task is done by the worker.
 // If the worker succeed it will mark the task done.
 // If the worker fails to do the task, it will retry with another worker
 // until reached to certern limit. Then it will mark the task failed.
