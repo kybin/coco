69631d839d23413d6de6a624ce839757b6d946e9
kim yongbin <kybinz@gmail.com>
2018-12-09 19:58:34 +0900

    code and comment some Task

--- task.go
+++ task.go
@@ -1,6 +1,30 @@
 package coco
 
-// A task is either a leaf task or a branch task.
+// Task is either a leaf task or a branch task.
+// If a task holds children, it is a branch task, or it is a leaf task.
+
+// Cmd is a command to be run.
+// First string is a command, and the others are arguments.
+// when a Cmd is empty or nil, the Cmd will skipped.
+type Cmd []string
+
+// CmdGroup is a bunch of commands that will run in a machine.
+// Task itself does not garantee that it is run inside of same machine.
+type CmdGroup []Cmd
+
+// Task's running order is as follows.
 //
-// A leaf task holds some commands that can handled by a worker.
-// A branch task holds other tasks.
+// PreCmd -> (Children's Commands) -> Cmd
+//
+// If one of the commands fails, task will also marked as fail,
+// and the following commands will not run.
+// Note that this does not support clean-up command
+// for making the process simple.
+// If you need this, please make your own batch script.
+type Task struct {
+	PreCmd CmdGroup
+	Cmd    CmdGroup
+
+	Children       []Task
+	SerialChildren bool // If true, it will run next child when prior child done.
+}
