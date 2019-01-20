3d763e686d7b57a703a929cdceed69d26dbab3ef
kim yongbin <kybinz@gmail.com>
2019-01-20 17:15:39 +0900

    cmd/cocofarm: create very basic root handler

--- cmd/cocofarm/main.go
+++ cmd/cocofarm/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"flag"
+	"net/http"
+)
+
 // Farm communicates both clients and workers.
 // It will keep watching jobs and workers, and match those.
 //
@@ -12,3 +17,18 @@ package main
 //
 // Farm also shows current status to it's web page.
 // The page should show all current jobs and workers and their status.
+
+func main() {
+	var (
+		addr string
+		cert string
+		key  string
+	)
+	flag.StringVar(&addr, "addr", ":3187", "https bind address")
+	flag.StringVar(&cert, "cert", "cert/cert.pem", "https certificate file")
+	flag.StringVar(&key, "key", "cert/key.pem", "https key file")
+	flag.Parse()
+
+	http.HandleFunc("/", rootHandler)
+	http.ListenAndServeTLS(addr, cert, key, nil)
+}
